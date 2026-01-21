b9d833a7db77cfd20d34c76d900d98c77a7d7fa1
CanSaragih <canwhardana@gmail.com>
2026-01-21 15:16:59 +0700

    chore: setup connection database supabase

--- weather-app/app/page.tsx
+++ weather-app/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <section className="min-h-screen mx-auto max-w-full py-4 px-4 sm:py-2 sm:px-4 md:px-8 lg:px-16 xl:px-10 2xl:px-20">
-      <h1 className="font-bold text-2xl text-zinc-100">Weather App</h1>
+    <section className="min-h-screen p-4">
+      <h1 className="font-bold text-2xl">Weather App</h1>
     </section>
   );
 }
