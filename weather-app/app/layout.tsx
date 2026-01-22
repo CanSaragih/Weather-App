1ccbf1860714b315272886a205a8387d8fe75841
CanSaragih <canwhardana@gmail.com>
2026-01-22 09:27:33 +0700

    feat: implement tooltip in toggle theme

--- weather-app/app/layout.tsx
+++ weather-app/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
+import { TooltipProvider } from "@/components/ui/tooltip";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +29,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider attribute="class" enableSystem defaultTheme="system">
+        <ThemeProvider
+          attribute="class"
+          enableSystem
+          defaultTheme="dark"
+          disableTransitionOnChange
+        >
           {children}
         </ThemeProvider>
       </body>
--- weather-app/components/ui/ThemeToggle.tsx
+++ weather-app/components/ui/ThemeToggle.tsx
@@ -3,23 +3,34 @@
 import { MdDarkMode, MdLightMode } from "react-icons/md";
 import { useTheme } from "next-themes";
 import { Button } from "./button";
+import { Tooltip, TooltipContent, TooltipTrigger } from "./tooltip";
+import { Kbd } from "./kbd";
 
 export default function ToggelButton() {
   const { setTheme } = useTheme();
 
   return (
     <div className="flex items-center border border-gray-200 dark:border-gray-700/70 rounded-md ">
-      <Button
-        variant="ghost"
-        size="icon"
-        className="cursor-pointer"
-        onClick={() =>
-          setTheme((prev) => (prev === "light" ? "dark" : "light"))
-        }
-      >
-        <MdDarkMode className="h-10 w-10 absolute rotate-100 scale-0 dark:rotate-0 dark:scale-100" />
-        <MdLightMode className="h-10 w-10 rotate-0 scale-100 dark:rotate-100 dark:scale-0" />
-      </Button>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button
+            variant="ghost"
+            size="icon"
+            className="cursor-pointer"
+            onClick={() =>
+              setTheme((prev) => (prev === "light" ? "dark" : "light"))
+            }
+          >
+            <MdDarkMode className="h-10 w-10 absolute rotate-100 scale-0 dark:rotate-0 dark:scale-100" />
+            <MdLightMode className="h-10 w-10 rotate-0 scale-100 dark:rotate-100 dark:scale-0" />
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent className="pr-1.5">
+          <div className="flex items-center gap-2">
+            Toggle Theme <Kbd>S</Kbd>
+          </div>
+        </TooltipContent>
+      </Tooltip>
     </div>
   );
 }
