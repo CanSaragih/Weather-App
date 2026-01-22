3c832bf28c7defd865124735eeefba55afaaa1bc
CanSaragih <canwhardana@gmail.com>
2026-01-22 09:54:00 +0700

    feat: impllement shortcut key in component toggle theme and search bar

--- weather-app/components/SearchBar.tsx
+++ weather-app/components/SearchBar.tsx
@@ -1,18 +1,48 @@
+"use client";
+
 import { Input } from "./ui/input";
 import { IoSearch } from "react-icons/io5";
 import ThemeToggle from "./ui/ThemeToggle";
+import { Kbd } from "./ui/kbd";
+import { MdKeyboardCommandKey } from "react-icons/md";
+import { useEffect, useRef } from "react";
 
 export default function SearchBar() {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    const handleKeysPress = (e: KeyboardEvent) => {
+      if (e.key.toLowerCase() === "k" && !e.ctrlKey && e.metaKey) {
+        const target = e.target as HTMLElement;
+        const isTyping = ["INPUT", "TEXTAREA", "SELECT"].includes(
+          target.tagName,
+        );
+        if (!isTyping) {
+          e.preventDefault();
+          inputRef.current?.focus();
+        }
+      }
+    };
+    window.addEventListener("keydown", handleKeysPress);
+    return () => {
+      window.removeEventListener("keydown", handleKeysPress);
+    };
+  }, []);
+
   return (
     <div className="fixed top-10 left-0 right-0 px-10">
       <div className="flex items-center gap-4 justify-end max-w-7xl ml-auto">
         <ThemeToggle />
         <div className="relative max-w-md w-full">
           <IoSearch className="absolute left-3 top-1/2 -translate-y-1/2 w-5 h-5 text-gray-400" />
+          <Kbd className="absolute right-3 top-1/2 -translate-y-1/2">
+            <MdKeyboardCommandKey className="size-3" /> K
+          </Kbd>
           <Input
+            ref={inputRef}
             type="search"
-            placeholder="search city.."
-            className="pl-10 w-full"
+            placeholder="Search city..."
+            className="pl-10 pr-16 w-full"
           />
         </div>
       </div>
--- weather-app/components/ui/ThemeToggle.tsx
+++ weather-app/components/ui/ThemeToggle.tsx
@@ -5,9 +5,34 @@ import { useTheme } from "next-themes";
 import { Button } from "./button";
 import { Tooltip, TooltipContent, TooltipTrigger } from "./tooltip";
 import { Kbd } from "./kbd";
+import { useEffect } from "react";
 
 export default function ToggelButton() {
-  const { setTheme } = useTheme();
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    const handleKeysPress = (e: KeyboardEvent) => {
+      if (
+        e.key.toLowerCase() === "d" &&
+        !e.ctrlKey &&
+        !e.altKey &&
+        !e.metaKey
+      ) {
+        const target = e.target as HTMLElement;
+        const isTyping = ["INPUT", "TEXTAREA", "SELECT"].includes(
+          target.tagName,
+        );
+        if (!isTyping) {
+          e.preventDefault();
+          setTheme(theme === "light" ? "dark" : "light");
+        }
+      }
+    };
+    window.addEventListener("keydown", handleKeysPress);
+    return () => {
+      window.removeEventListener("keydown", handleKeysPress);
+    };
+  }, [theme, setTheme]);
 
   return (
     <div className="flex items-center border border-gray-200 dark:border-gray-700/70 rounded-md ">
@@ -17,9 +42,7 @@ export default function ToggelButton() {
             variant="ghost"
             size="icon"
             className="cursor-pointer"
-            onClick={() =>
-              setTheme((prev) => (prev === "light" ? "dark" : "light"))
-            }
+            onClick={() => setTheme(theme === "light" ? "dark" : "light")}
           >
             <MdDarkMode className="h-10 w-10 absolute rotate-100 scale-0 dark:rotate-0 dark:scale-100" />
             <MdLightMode className="h-10 w-10 rotate-0 scale-100 dark:rotate-100 dark:scale-0" />
@@ -27,7 +50,7 @@ export default function ToggelButton() {
         </TooltipTrigger>
         <TooltipContent className="pr-1.5">
           <div className="flex items-center gap-2">
-            Toggle Theme <Kbd>S</Kbd>
+            Toggle Theme <Kbd>D</Kbd>
           </div>
         </TooltipContent>
       </Tooltip>
